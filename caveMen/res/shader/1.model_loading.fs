fd6f57013e3590a5a28ea27a4bebb01ef4c56320
haoxiaoshuai <wusuopu@zdd.space>
2021-07-28 13:54:18 +0800

    Update 1.model_loading.fs

--- caveMen/res/shader/1.model_loading.fs
+++ caveMen/res/shader/1.model_loading.fs
@@ -4,11 +4,11 @@ out vec4 FragColor;
 in vec2 TexCoord;
 
 // texture samplers
-uniform sampler2D texture1;
-uniform sampler2D texture2;
+uniform sampler2D texture_diffuse1;
+//uniform sampler2D texture_specular1;
 
 void main()
 {
 	// linearly interpolate between both textures (80% container, 20% awesomeface)
-	FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2);
+	FragColor = texture(texture_diffuse1, TexCoord);
 }
