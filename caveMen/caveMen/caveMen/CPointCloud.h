48e663562f61eb673d42793bff725978de611276
haoxiaoshuai <wusuopu@zdd.space>
2021-07-30 18:10:21 +0800

    1

--- caveMen/caveMen/caveMen/CPointCloud.h
+++ caveMen/caveMen/caveMen/CPointCloud.h
@@ -5,6 +5,16 @@ class CPointCloud :
 {
 public:
 	CPointCloud();
-	
+	void updateVectorCloud();
+	void Draw();
+	// mesh Data
+	std::vector<SVertex>       vertices;
+	std::vector<unsigned int>  indices;
+	unsigned int VAO;
+	// render data 
+	unsigned int VBO, EBO;
+
+	// initializes all the buffer objects/arrays
+	void setupMesh();
 };
 
